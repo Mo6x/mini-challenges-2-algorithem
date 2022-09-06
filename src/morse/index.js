f5d6d1645f355e9bfd9905dc23d882b1d562e58e
christopher-moses <christmoses1999@icloud.com>
2022-09-06 07:08:52 +0100

    first commit

---
+++ src/morse/index.js
@@ -0,0 +1,94 @@
+const MORSE_CODE = {
+  "-.-.--": "!",
+  ".-..-.": '"',
+  "...-..-": "$",
+  ".-...": "&",
+  ".----.": "'",
+  "-.--.": "(",
+  "-.--.-": ")",
+  ".-.-.": "+",
+  "--..--": ",",
+  "-....-": "-",
+  ".-.-.-": ".",
+  "-..-.": "/",
+  "-----": "0",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "---...": ":",
+  "-.-.-.": ";",
+  "-...-": "=",
+  "..--..": "?",
+  ".--.-.": "@",
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  "..--.-": "_",
+  "...---...": "SOS",
+};
+
+Object.freeze(MORSE_CODE);
+
+function morse(text) {
+  function morse(text) {}
+function morse(text) {
+
+  if (text === "") return '';
+  if (typeof text !== 'string') throw new Error('Please provide a morse string');
+
+const input = text.trim();
+const words = input.split(/\s{3,}/);
+
+let result = '';
+
+words.forEach((word) => {
+  word.split(" ").forEach((char) => {
+  result += MORSE_CODE[char]
+});
+  result += ' ';
+})
+
+for (let i = 0; i < words.length; i++) {
+  let word = words[i]
+  let characters = word.split(' ');
+for(let j = 0; j < characters.length; j++) {
+  let char = characters[j];
+}
+}
+
+  return result.trim()
+}
+
+
+}
+
+module.exports = morse;
